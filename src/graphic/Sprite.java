9a83ecf18e3d0a0c97dbfaee190af32f7b4e616f
Davide <64044800+Dr-lvr@users.noreply.github.com>
2021-02-01 11:15:15 +0100

    tokenRing icons adjustement

--- src/graphic/Sprite.java
+++ src/graphic/Sprite.java
@@ -12,11 +12,16 @@ public class Sprite extends JComponent {
 
     public void paint(Graphics g) {
         Graphics2D g2 = (Graphics2D) g;
+
+        //load image in cache
         Image img1 = Toolkit.getDefaultToolkit().getImage("src/a_images/computer-icon.png");
 
         //create the network configuration
+        //draw incon
         g2.drawImage(img1, (this.getWidth()/2)-50, 10, 100, 100, this);
+        //set graphical wiring
         g2.drawLine((this.getWidth()/2)-50,(this.getHeight()/4)-50, (this.getWidth()/4)-60, (this.getHeight()/2)-50);
+
         g2.drawImage(img1, (this.getWidth()/4)-130, (this.getWidth()/2)-110, 100, 100, this);
         g2.drawImage(img1, (this.getWidth()/4)*3, (this.getWidth()/2)-110, 100, 100, this);
         g2.drawImage(img1, (this.getWidth()/2)-50, (this.getWidth()/4)*3-110, 100, 100, this);
