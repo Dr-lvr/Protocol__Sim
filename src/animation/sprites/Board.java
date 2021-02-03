70a269d54fea90b12d2d31c1caee05a8171b27c3
Davide <64044800+Dr-lvr@users.noreply.github.com>
2021-02-03 01:15:32 +0100

    Start implementing token ring

--- src/animation/sprites/Board.java
+++ src/animation/sprites/Board.java
@@ -14,6 +14,7 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Vector;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
@@ -21,15 +22,16 @@ public class Board extends JPanel implements ActionListener {
 
     Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
     private Timer timer;
-    private Graphical_Computer graphical_computer;
-    private List<Graphical_Computer> theNetwork;
+    //private Graphical_Computer graphical_computer;
+    //private List<Graphical_Computer> theNetwork;
+    private Vector<Graphical_Computer> graphical_computer;
     private boolean isRunning;
     private final int ICRAFT_X = 40;
     private final int ICRAFT_Y = 60;
     private final int B_WIDTH = (int) screenSize.getWidth();
     private final int B_HEIGHT = (int) screenSize.getHeight();
     private final int DELAY = 15;
-
+/*
     private final int[][] pos = {
             {2380, 29}, {2500, 59}, {1380, 89},
             {780, 109}, {580, 139}, {680, 239},
@@ -41,7 +43,7 @@ public class Board extends JPanel implements ActionListener {
             {810, 220}, {860, 20}, {740, 180},
             {820, 128}, {490, 170}, {700, 30}
     };
-
+*/
     public Board() {
 
         initBoard();
@@ -56,22 +58,23 @@ public class Board extends JPanel implements ActionListener {
 
         setPreferredSize(new Dimension(B_WIDTH, B_HEIGHT));
 
-        graphical_computer = new Graphical_Computer(ICRAFT_X, ICRAFT_Y);
-
-        initNetwork();
+        //graphical_computer = new Graphical_Computer(ICRAFT_X, ICRAFT_Y);
+        graphical_computer = ConfigProvider.getControllerInstance().getTokenRing();
+       // initNetwork();
 
         timer = new Timer(DELAY, this);
         timer.start();
     }
 
-    public void initNetwork() {
+   // public void initNetwork() {
 
-        theNetwork = new ArrayList<>();
-
+        //theNetwork = new ArrayList<>();
+        //theNetwork = ConfigProvider.getControllerInstance().getTokenRing();
+        /*
         for (int[] p : pos) {
             theNetwork.add(new Graphical_Computer(p[0], p[1]));
-        }
-    }
+        }*/
+    //}
 
     @Override
     public void paintComponent(Graphics g) {
@@ -91,28 +94,32 @@ public class Board extends JPanel implements ActionListener {
 
     private void drawObjects(Graphics g) {
 
-        if (graphical_computer.isVisible()) {
-            g.drawImage(graphical_computer.getImage(), graphical_computer.getX(), graphical_computer.getY(),
-                    this);
-        }
+        // draw network
+        for(Graphical_Computer g_cp : graphical_computer) {
 
-        List<Graphical_Package> ms = graphical_computer.getPackages();
+            if (g_cp.isVisible()) {
+                g.drawImage(g_cp.getImage(), g_cp.getX(), g_cp.getY(),
+                        this);
+            }
 
-        for (Graphical_Package aPackage : ms) {
-            if (aPackage.isVisible()) {
-                g.drawImage(aPackage.getImage(), aPackage.getX(),
-                        aPackage.getY(), this);
+            List<Graphical_Package> ms = g_cp.getPackages();
+
+            for (Graphical_Package aPackage : ms) {
+                if (aPackage.isVisible()) {
+                    g.drawImage(aPackage.getImage(), aPackage.getX(),
+                            aPackage.getY(), this);
+                }
             }
         }
 
-        for (Graphical_Computer graphical_computer : theNetwork) {
+        for (Graphical_Computer graphical_computer : graphical_computer) {
             if (graphical_computer.isVisible()) {
                 g.drawImage(graphical_computer.getImage(), graphical_computer.getX(), graphical_computer.getY(), this);
             }
         }
 
         g.setColor(Color.WHITE);
-        g.drawString("Computers left: " + theNetwork.size(), 5, 15);
+        g.drawString("Computers left: " + graphical_computer.size(), 5, 15);
     }
 
     private void drawGameOver(Graphics g) {
@@ -150,78 +157,85 @@ public class Board extends JPanel implements ActionListener {
 
     private void updateNetwork() {
 
-        if (graphical_computer.isVisible()) {
+        for(Graphical_Computer g_cp : graphical_computer) {
+            if (g_cp.isVisible()) {
 
-            graphical_computer.move();
+                g_cp.move();
+            }
         }
     }
 
     private void updatePackage() {
 
-        List<Graphical_Package> ms = graphical_computer.getPackages();
+        for(Graphical_Computer g_cp : graphical_computer) {
+            List<Graphical_Package> ms = g_cp.getPackages();
 
-        for (int i = 0; i < ms.size(); i++) {
+            for (int i = 0; i < ms.size(); i++) {
 
-            Graphical_Package m = ms.get(i);
+                Graphical_Package m = ms.get(i);
 
-            if (m.isVisible()) {
-                m.move();
-            } else {
-                ms.remove(i);
+                if (m.isVisible()) {
+                    m.move();
+                } else {
+                    ms.remove(i);
+                }
             }
         }
     }
 
     private void updateComputerUnits() {
 
-        if (theNetwork.isEmpty()) {
+        if (graphical_computer.isEmpty()) {
 
             isRunning = false;
             return;
         }
 
-        for (int i = 0; i < theNetwork.size(); i++) {
+        for (int i = 0; i < graphical_computer.size(); i++) {
 
-            Graphical_Computer a = theNetwork.get(i);
+            Graphical_Computer a = graphical_computer.get(i);
 
             if (a.isVisible()) {
                 a.move();
             } else {
-                theNetwork.remove(i);
+                graphical_computer.remove(i);
             }
         }
     }
 
     public void checkCollisions() {
-
+        /* collision between machines
         Rectangle r3 = graphical_computer.getBounds();
 
-        for (Graphical_Computer alien : theNetwork) {
+        for (Graphical_Computer computerUnit : theNetwork) {
 
-            Rectangle r2 = alien.getBounds();
+            Rectangle r2 = computerUnit.getBounds();
 
             if (r3.intersects(r2)) {
 
                 graphical_computer.setVisible(false);
-                alien.setVisible(false);
+                computerUnit.setVisible(false);
                 isRunning = false;
             }
-        }
+        }*/
 
-        List<Graphical_Package> ms = graphical_computer.getPackages();
+        for(Graphical_Computer g_cp : graphical_computer) {
+            List<Graphical_Package> ms = g_cp.getPackages();
 
-        for (Graphical_Package m : ms) {
+            for (Graphical_Package m : ms) {
 
-            Rectangle r1 = m.getBounds();
+                Rectangle r1 = m.getBounds();
 
-            for (Graphical_Computer alien : theNetwork) {
+                for (Graphical_Computer computerUnit : graphical_computer) {
 
-                Rectangle r2 = alien.getBounds();
+                    Rectangle r2 = computerUnit.getBounds();
 
-                if (r1.intersects(r2)) {
+                    if (r1.intersects(r2)) {
 
-                    m.setVisible(false);
-                    alien.setVisible(false);
+                        m.setVisible(false);
+                        //computerUnit.setVisible(false);
+                        computerUnit.fire();
+                    }
                 }
             }
         }
@@ -231,12 +245,12 @@ public class Board extends JPanel implements ActionListener {
 
         @Override
         public void keyReleased(KeyEvent e) {
-            graphical_computer.keyReleased(e);
+            graphical_computer.get(0).keyReleased(e);
         }
 
         @Override
         public void keyPressed(KeyEvent e) {
-            graphical_computer.keyPressed(e);
+            graphical_computer.get(0).keyPressed(e);
         }
     }
 }
